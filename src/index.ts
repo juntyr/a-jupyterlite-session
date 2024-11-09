10db8794a1daf917f9b7bad43e6011ea9b48faf7
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2024-11-09 21:39:06 +0000

    Wait for ready contents

--- src/index.ts
+++ src/index.ts
@@ -14,66 +14,68 @@ const plugin: JupyterLiteServerPlugin<void> = {
   id: 'a-jupyterlab-session:plugin',
   autoStart: true,
   requires: [IContents],
-  activate: async (app: JupyterLiteServer, contents: IContents) => {
+  activate: (app: JupyterLiteServer, contents: IContents) => {
     const SESSIONS = '.sessions';
     const REQUIREMENTS = 'requirements.txt';
 
-    console.log(
-      'JupyterLite server extension a-jupyterlab-session is activated!'
-    );
+    contents.ready.then(async () => {
+      console.log(
+        'JupyterLite server extension a-jupyterlab-session is activated!'
+      );
 
-    if (!(await contents.get(SESSIONS, { content: false }))) {
+      if (!(await contents.get(SESSIONS, { content: false }))) {
+        await contents
+          .newUntitled({
+            type: 'directory' as Contents.ContentType,
+            path: PathExt.dirname(SESSIONS)
+          })
+          .then(async directory => {
+            await contents.rename(directory!.path, SESSIONS);
+          });
+      }
+
+      // Generate a '%dd.%mm.%yyyy-%hh:%mm:%ss' timestamp
+      const timestamp = new Date()
+        .toLocaleDateString('en-us', {
+          year: 'numeric',
+          month: '2-digit',
+          day: '2-digit',
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit',
+          hourCycle: 'h24'
+        })
+        .replace(/\//g, '.')
+        .replace(', ', '-');
+      const salt = uuidv4().slice(0, 8);
+
+      // Generate a unique session directory name with the current time and a random suffix
+      const session = PathExt.join(SESSIONS, `${timestamp}-${salt}`);
+
+      // Create the new directory
       await contents
         .newUntitled({
           type: 'directory' as Contents.ContentType,
-          path: PathExt.dirname(SESSIONS)
+          path: PathExt.dirname(session)
         })
         .then(async directory => {
-          await contents.rename(directory!.path, SESSIONS);
+          await contents.rename(directory!.path, session);
         });
-    }
 
-    // Generate a '%dd.%mm.%yyyy-%hh:%mm:%ss' timestamp
-    const timestamp = new Date()
-      .toLocaleDateString('en-us', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hourCycle: 'h24'
-      })
-      .replace(/\//g, '.')
-      .replace(', ', '-');
-    const salt = uuidv4().slice(0, 8);
-
-    // Generate a unique session directory name with the current time and a random suffix
-    const session = PathExt.join(SESSIONS, `${timestamp}-${salt}`);
-
-    // Create the new directory
-    await contents
-      .newUntitled({
-        type: 'directory' as Contents.ContentType,
-        path: PathExt.dirname(session)
-      })
-      .then(async directory => {
-        await contents.rename(directory!.path, session);
+      // Navigate the filebrowser to the new session directory
+      await app.commands.execute('filebrowser:open-path', {
+        path: session
       });
 
-    // Navigate the filebrowser to the new session directory
-    await app.commands.execute('filebrowser:open-path', {
-      path: session
+      // Copy the current requirements.txt file to the new session folder
+      await contents
+        .copy(REQUIREMENTS, session)
+        .catch(reason =>
+          console.warn(
+            `Failed to copy the ${REQUIREMENTS} file to the new session: ${reason}`
+          )
+        );
     });
-
-    // Copy the current requirements.txt file to the new session folder
-    await contents
-      .copy(REQUIREMENTS, session)
-      .catch(reason =>
-        console.warn(
-          `Failed to copy the ${REQUIREMENTS} file to the new session: ${reason}`
-        )
-      );
   }
 };
 
