009a8c7ba419531cd5e362deb2361bf21bcaa15e
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2025-03-21 12:17:10 +0200

    Upgrade extension to a JupyterFrontEndPlugin (#1)
    * Start upgrading to a JupyterFrontEndPlugin
    
    * Provide the session path as a token

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,40 @@
-import { PathExt } from '@jupyterlab/coreutils';
-import { IContents } from '@jupyterlite/contents';
 import {
-  JupyterLiteServer,
-  JupyterLiteServerPlugin,
-  Router
-} from '@jupyterlite/server';
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import { PathExt } from '@jupyterlab/coreutils';
+import { BrowserStorageDrive } from '@jupyterlite/contents';
+import { IDefaultDrive, Contents } from '@jupyterlab/services';
 import { v4 as uuidv4 } from 'uuid';
+
+import { IJupyterLiteSession } from './tokens';
 
 /**
  * Initialization data for the a-jupyterlite-session extension.
  */
-const plugin: JupyterLiteServerPlugin<void> = {
+const plugin: JupyterFrontEndPlugin<IJupyterLiteSession> = {
   id: 'a-jupyterlite-session:plugin',
   autoStart: true,
-  requires: [IContents],
-  activate: (app: JupyterLiteServer, contents: IContents) => {
+  provides: IJupyterLiteSession,
+  requires: [IDefaultDrive],
+  activate: async (_app: JupyterFrontEnd, idrive: Contents.IDrive) => {
     const SESSIONS = '.sessions';
     const README = 'README.md';
     const REQUIREMENTS = 'requirements.txt';
-    const API_ENDPOINT = '/api/a-session';
 
-    contents.ready.then(async () => {
-      console.log(
-        'JupyterLite server extension a-jupyterlite-session is activated!'
-      );
+    const drive = idrive as BrowserStorageDrive;
+    await drive.ready;
 
-      const storage = (await (contents as any).storage) as LocalForage;
+    console.log('JupyterLite extension a-jupyterlite-session is activated!');
 
-      const now = new Date();
+    const storage = (drive as any)._storage as LocalForage;
 
-      if (!(await storage.getItem(SESSIONS))) {
-        await storage.setItem(SESSIONS, {
-          name: SESSIONS,
-          path: SESSIONS,
-          last_modified: now.toISOString(),
-          created: now.toISOString(),
-          format: 'json',
-          mimetype: '',
-          content: null,
-          size: 0,
-          writable: true,
-          type: 'directory'
-        });
-      }
+    const now = new Date();
 
-      // Generate a '%dd-%mm-%yyyy-%hh-%mm-%ss' timestamp
-      const timestamp = now
-        .toLocaleDateString('en-us', {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit',
-          hour: '2-digit',
-          minute: '2-digit',
-          second: '2-digit',
-          hourCycle: 'h24'
-        })
-        .replace(/\//g, '-')
-        .replace(/:/g, '-')
-        .replace(', ', '-');
-      const salt = uuidv4().slice(0, 8);
-
-      // Generate a unique session directory name with the current time and a
-      //  random suffix
-      const session = PathExt.join(SESSIONS, `${timestamp}-${salt}`);
-
-      // Create the new directory
-      await storage.setItem(session, {
-        name: PathExt.basename(session),
-        path: session,
+    if (!(await storage.getItem(SESSIONS))) {
+      await storage.setItem(SESSIONS, {
+        name: SESSIONS,
+        path: SESSIONS,
         last_modified: now.toISOString(),
         created: now.toISOString(),
         format: 'json',
@@ -77,41 +44,72 @@ const plugin: JupyterLiteServerPlugin<void> = {
         writable: true,
         type: 'directory'
       });
+    }
 
-      // Share the session folder location with the `a-jupyterlab-session`
-      //  frontend extension
-      app.router.get(
-        API_ENDPOINT,
-        async (_req: Router.IRequest) => new Response(session)
+    // Generate a '%dd-%mm-%yyyy-%hh-%mm-%ss' timestamp
+    const timestamp = now
+      .toLocaleDateString('en-us', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hourCycle: 'h24'
+      })
+      .replace(/\//g, '-')
+      .replace(/:/g, '-')
+      .replace(', ', '-');
+    const salt = uuidv4().slice(0, 8);
+
+    // Generate a unique session directory name with the current time and a
+    //  random suffix
+    const sessionPath = PathExt.join(SESSIONS, `${timestamp}-${salt}`);
+
+    // Create the new directory
+    await storage.setItem(sessionPath, {
+      name: PathExt.basename(sessionPath),
+      path: sessionPath,
+      last_modified: now.toISOString(),
+      created: now.toISOString(),
+      format: 'json',
+      mimetype: '',
+      content: null,
+      size: 0,
+      writable: true,
+      type: 'directory'
+    });
+
+    // If litegitpuller is used to load a repo, set its uploadpath to the
+    //  session folder
+    const url = new URL(window.location.href);
+    if (url.searchParams.get('repo') !== null) {
+      url.searchParams.set('uploadpath', sessionPath);
+      window.history.replaceState(null, '', url);
+    }
+
+    // Copy the current README.md file to the new session folder, without awaiting it
+    drive
+      .copy(README, sessionPath)
+      .catch(reason =>
+        console.warn(
+          `Failed to copy the ${README} file to the new session: ${reason}`
+        )
       );
 
-      // If litegitpuller is used to load a repo, set its uploadpath to the
-      //  session folder
-      const url = new URL(window.location.href);
-      if (url.searchParams.get('repo') !== null) {
-        url.searchParams.set('uploadpath', session);
-        window.history.replaceState(null, '', url);
-      }
+    // Copy the current requirements.txt file to the new session folder, without awaiting it
+    drive
+      .copy(REQUIREMENTS, sessionPath)
+      .catch(reason =>
+        console.warn(
+          `Failed to copy the ${REQUIREMENTS} file to the new session: ${reason}`
+        )
+      );
 
-      // Copy the current README.md file to the new session folder
-      await contents
-        .copy(README, session)
-        .catch(reason =>
-          console.warn(
-            `Failed to copy the ${README} file to the new session: ${reason}`
-          )
-        );
-
-      // Copy the current requirements.txt file to the new session folder
-      await contents
-        .copy(REQUIREMENTS, session)
-        .catch(reason =>
-          console.warn(
-            `Failed to copy the ${REQUIREMENTS} file to the new session: ${reason}`
-          )
-        );
-    });
+    return { sessionPath };
   }
 };
 
 export default plugin;
+
+export * from './tokens';
